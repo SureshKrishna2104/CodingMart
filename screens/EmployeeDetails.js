011794a3609520505df632f620a7d3f81208de04
SureshKrishna2104 <57995524+SureshKrishna2104@users.noreply.github.com>
2021-06-08 22:05:21 +0530

    Update EmployeeDetails.js
    Allign issues

--- screens/EmployeeDetails.js
+++ screens/EmployeeDetails.js
@@ -14,11 +14,21 @@ import {
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import DropDownPicker from 'react-native-dropdown-picker';
-const EmployeeDetail = () => {
+const EmployeeDetail = props => {
   const [ename, setEname] = React.useState('');
   const [eroles, setEroles] = React.useState('');
   const [elead, setElead] = React.useState('');
+  const [namearray, setEnamearray] = React.useState([]);
+  const [rolearray, setErolearray] = React.useState([]);
+  const [leadarray, setEleadarray] = React.useState([]);
+
   useEffect(() => {
+    const name = props.navigation.getParam('user');
+    setEnamearray(name);
+    const role = props.navigation.getParam('role');
+    setErolearray(role);
+    const lead = props.navigation.getParam('role');
+    setEleadarray(lead);
     AsyncStorage.getItem('Name').then(async res => {
       console.warn('name', ename);
 
@@ -37,13 +47,26 @@ const EmployeeDetail = () => {
       const elead = await res;
       setElead(elead);
     });
+    AsyncStorage.getItem('Namearr').then(async res => {
+      console.warn('Namearr', res);
+
+      enamea.push(JSON.stringify(res));
+      console.warn('Namearr', enamea);
+      //const elead = await res;
+      //setElead(elead);
+    });
   }, []);
   return (
     <View>
-      <Text>Employee Details</Text>
-      <Text>EmployeeName:{ename}</Text>
-      <Text>EmployeeRoles:{eroles}</Text>
-      <Text>EmployeeLead:{elead}</Text>
+      {namearray.map((item, key) => (
+        <Text key={key}>EmployeeName:{item}</Text>
+      ))}
+      {rolearray.map((item, key) => (
+        <Text key={key}>EmployeeRole:{item}</Text>
+      ))}
+      {leadarray.map((item, key) => (
+        <Text key={key}>EmployeeLead:{item}</Text>
+      ))}
     </View>
   );
 };
